5620422a32676c7291ab0bc071775a310622781a
NGO NHUT SANG <sangngo106@gmail.com>
2026-07-25 19:19:38 +0700

    Test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -121,19 +121,19 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0,1);
-	int s = 100 ;
-  while (1)
-  {
-    /* USER CODE END WHILE */
+// 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0,1);
+// 	int s = 100 ;
+//   while (1)
+//   {
+//     /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-//		HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13);
-//		HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_1);
-//		HAL_Delay(s);
-		writeDataSPI(0xFF);
-		HAL_Delay(500);
-  }
+//     /* USER CODE BEGIN 3 */
+// //		HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13);
+// //		HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_1);
+// //		HAL_Delay(s);
+// 		writeDataSPI(0xFF);
+// 		HAL_Delay(500);
+//   }
   /* USER CODE END 3 */
 }
 
